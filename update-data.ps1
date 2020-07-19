e1f73203a80758396019e62a1ae1678f0c385e4c
Scott Gentzen <sgentzen@gmail.com>
2020-07-19 12:43:44 -0400

    clip trailing slash

--- update-data.ps1
+++ update-data.ps1
@@ -1,8 +1,8 @@
 #data setup
 $scriptDir = $(get-location)
-$virginiaCasesDataUrl = "https://www.vdh.virginia.gov/content/uploads/sites/182/2020/05/"
+$virginiaCasesDataUrl = "https://www.vdh.virginia.gov/content/uploads/sites/182/2020/05"
 $virginiaCasesDataFile = "VDH-COVID-19-PublicUseDataset-Cases.csv"
-$marylandCasesDataUrl = "https://opendata.arcgis.com/datasets/"
+$marylandCasesDataUrl = "https://opendata.arcgis.com/datasets"
 $marylandCasesDataFile = "18582de727934249b92c52542395a3bf_0.csv"
 $districtCasesDataUrl = ""
 $districtCasesDataUrl = ""
